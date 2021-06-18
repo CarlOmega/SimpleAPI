be271b6e9b93b24ebf19a2e157439037d97db166
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-18 13:58:28 +1000

    fixs: local config
    Ignore local runtime added.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.get('/', async (req, res) => {
   try {
     const userRecored = await admin.auth().getUserByEmail("carl.w.humphries@gmail.com");
-    res.status(200).send(`Hey there ${userRecored.email}!`);
+    res.status(200).send(`Hey there ${userRecored.email}?!`);
 
   } catch (error) {
     res.status(500).send(error);
