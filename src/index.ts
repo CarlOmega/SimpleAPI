c37fee398be34aef3efc9f2b97c6472f01c7db76
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-18 13:38:28 +1000

    feat: admin sdk setup for firebase
    Using firebase config setup admin sdk for blaze
    plan app.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import { admin } from './config/firebase';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const userRecored = await admin.auth().getUserByEmail("carl.w.humphries@gmail.com");
+    res.status(200).send(`Hey there ${userRecored.email}!`);
+
+  } catch (error) {
+    res.status(500).send(error);
+    return;
+  }
+
+  
 });
+
+
+exports.app = functions.https.onRequest(app);
+
