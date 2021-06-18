e1f8ac1ac196afd5f04b2e3226a80f8a691c5e90
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-18 15:02:03 +1000

    feat: skeleton of enpoints
    Going to pause this and focus on user structure first.

--- src/controllers/restaurants.ts
+++ src/controllers/restaurants.ts
@@ -5,12 +5,33 @@ import { authEndpoint } from "../middleware/authentication";
 const RestaurantController = Router();
 const db = admin.firestore().collection("restaurants")
 
+// Using CRUD
+
+RestaurantController.post("/", authEndpoint, async (req: Request, res: Response) => {
+  // Create new resturants if owner
+  res.status(500);
+});
+
 RestaurantController.get("/", authEndpoint, async (req: Request, res: Response) => {
+  // Get all resturants if normal user or admin
+  // Return only owned resturants if owner claim
+
   const querySnapshot = await db.get();
   const data = querySnapshot.docs.map((doc) => doc.data());
 
   res.status(200).send(data);
 
+});
+
+RestaurantController.put("/", authEndpoint, async (req: Request, res: Response) => {
+  // Create edit owned resturants if owner
+  // Admin can edit any
+  res.status(500);
+});
+
+RestaurantController.delete("/", authEndpoint, async (req: Request, res: Response) => {
+  // Admin can delete any
+  res.status(500);
 });
 
 export default RestaurantController;
