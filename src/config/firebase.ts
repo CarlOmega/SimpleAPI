3a36fcd04caeaba5fb11ad84e042931cb8d21c03
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-18 14:26:57 +1000

    feat: auth middleware
    Using valid JWT auth endpoint to make sure the user
    is there and authenticated. can now secure
    endpoints and use claims to add extra security.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
-import * as admin from 'firebase-admin'
-import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -8,7 +8,6 @@ admin.initializeApp({
     clientEmail: functions.config().client.email
   }),
   databaseURL: 'https://toptal-d15b0.firebaseio.com'
-})
+});
 
-const db = admin.firestore()
-export { admin, db }
+export { admin }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import { admin } from './config/firebase';
+import { authEndpoint } from './middleware/authentication';
 
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/', authEndpoint, async (req, res) => {
   try {
-    const userRecored = await admin.auth().getUserByEmail("carl.w.humphries@gmail.com");
-    res.status(200).send(`Hey there ${userRecored.email}?!`);
-
+    res.status(200).send(`Hey there ${JSON.stringify(req.user)}?!`);
   } catch (error) {
     res.status(500).send(error);
     return;
   }
-
   
 });
-
 
 exports.app = functions.https.onRequest(app);
 
