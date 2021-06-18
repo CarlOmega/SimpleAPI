0ac9526d0c8531dcbe9a80c4dd057d8499757fe6
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-18 14:52:55 +1000

    fixs: router reconfiguration
    Refactor to clean up router.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import RestaurantController from './controllers/restaurants';
+import router from './router';
 
 const app = express();
-const router = express.Router();
-
-router.use('/restaurants', RestaurantController);
 
 app.use('/', router);
 
