703811fa2739ff0da923724c76d0b3072954f9a6
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-22 12:57:58 +1000

    feat: update and delete endpoints
    Admin can update email/username of any account.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -19,26 +19,47 @@ UserController.post("/", authEndpoint, async (req: Request, res: Response) => {
     await admin.auth().updateUser(userClaims.uid, {displayName: user.userName});
     return res.status(201).send({message: "Success setting up account"});
   } catch (error) {
-    return res.status(501).send({message: error.message});
+    return res.status(500).send({message: error.message});
   }
-});
-
-UserController.get("/", authEndpoint, async (req: Request, res: Response) => {
-  // Get current user else if admin get ever user
-
-  // Place holder taken from restaurants
-  res.status(200).send({});
-
 });
 
 UserController.put("/", authEndpoint, async (req: Request, res: Response) => {
   // Admin seems to be only one who can update accounts
-  res.status(500);
+  if (!req.user.admin)
+    return res.status(403).send({message: "Cannot perform action"});
+  
+  const uid = req.query.uid as string;
+  const user = req.body.user;
+
+  if (!uid) return res.status(400).send({message: "No uid in query"});
+  if (!user) return res.status(400).send({message: "No user in body"});
+
+  try {
+    const firebaseUser = await admin.auth().getUser(uid);
+    if (user.email && user.email.toLowerCase() !== firebaseUser.email)
+      await admin.auth().updateUser(uid, {email: user.email.toLowerCase()});
+    if (user.userName && user.userName !== firebaseUser.displayName)
+      await admin.auth().updateUser(uid, {displayName: user.userName});
+    return res.status(200).send({message: "Successful update"});
+  } catch (error) {
+    return res.status(500).send({message: error.message});
+  }
+  
 });
 
 UserController.delete("/", authEndpoint, async (req: Request, res: Response) => {
   // Admin can delete any accounts
-  res.status(500);
+  if (!req.user.admin)
+    return res.status(403).send({message: "Cannot perform action"});
+  
+  const uid = req.query.uid as string;
+  try {
+    await admin.auth().deleteUser(uid);
+    return res.status(200).send({message: "Successful delete"});
+  } catch (error) {
+    return res.status(500).send({message: error.message});
+  }
+
 });
 
 export default UserController;
